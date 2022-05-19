887e1ad31c3d6f55036c2c07093ad69d51335437
Fan <aliciafzhang@gmail.com>
2022-05-19 16:30:14 -0700

    react app and get server index file

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,15 +1,76 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+// import Overview from './Overview/Overview.jsx';
+// import Reviews from './Reviews/Reviews.jsx';
+// import QandAs from './QandAs/QandAs.jsx';
+// import RelatedItems from './RelatedItems/RelatedItems.jsx';
 
-class App extends React.Component {
-  constructor(props){
-    super(props);
+import axios from 'axios';
+function App () {
+  // Remember to change individual github tokens when you pull down the repo to your local!
+  const[someState, setStatefn] = useState({
+    products:[{'name': 'LL'}],
+    product_id: '40344'
+
+  })
+
+
+  const onLoad = () => {
+    axios.get('/products')
+      .then((response) =>{setStatefn({products: response.data}); console.log('products', response.data)})
+      .catch(err => console.log(err));
+
+      // var reviews = [];
+      // someState.products.map((product)=> axios.get(`/reviews/?id=${product['id']}`)
+      //   .then((response)=>reviews.push(response.data))
+      //   .catch(err => console.log(err)));
+
+      // setStatefn({reviews: reviews});
   }
 
-  render() {
-    return (
-      <div>Hello World</div>
-    );
-  }
+
+
+  useEffect(onLoad, []);
+
+
+
+  return(
+    <div>{someState.products[0]['name']}
+    {/* {someState.reviews[0]['count']} */}
+
+      <Overview product = {someState.products}/>
+      <Reviews product_id = {someState.product_id} />
+      <QandAs product_id = {someState.product_id}/>
+      <RelatedItems product_id = {someState.product_id}/>
+    </div>
+  );
 }
 
-  export default App;
+/*
+function App() {
+  const[someState, setStatefn] = useState({
+    person: 'Barry'
+  })
+  useEffect(() => {
+    // Update the document title using the browser API
+    document.title = `You clicked hello times`;
+  });
+  var newvar = "Barry";
+
+  const changeHandler = (e) => {
+    setStatefn({person: e.target.value});
+  }
+
+  const clickHandler = (e) => {
+    e.preventDefault();
+  }
+
+  return (
+      <div>
+      <div>Hello World</div>
+      <input onChange = {(e) => changeHandler(e)} value = {someState.person}></input>
+      <button onClick = {(e) => clickHandler(e)}>Click</button>
+      </div>
+  );
+}
+*/
+export default App;
