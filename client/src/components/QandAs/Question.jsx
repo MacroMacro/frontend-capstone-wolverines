0246b79c5b26c9feaf9febd07cd893aaef6d30b8
Jerry Tapia <jerryxtapia@gmail.com>
2022-05-24 18:03:13 -0700

    end of day 2

---
+++ client/src/components/QandAs/Question.jsx
@@ -0,0 +1,103 @@
+import React, { useState } from 'react';
+
+
+const Question = ({ key, question }) => {
+
+  const [showMoreAs, setShowMoreAs] = useState(false);
+
+  const changeQuestions = () => {
+    setShowMoreAs(!showMoreAs);
+  }
+
+  return (
+    <>
+      <div style={{ display: "flex", justifyContent: 'space-between' }}>
+        <h2 style={{ margin: 0 }}>
+          Q:{' '}{question.question_body}
+        </h2>
+        <div style={{ marginTop: '6px' }}>
+          <span style={{paddingRight: '10px'}}>
+            Helpful?
+            {' '}
+            <span type='button' onClick={()=>{console.log('click')}} style={{ textDecoration: "underline" }}>Yes</span>
+            {` (${question.question_helpfulness})`}
+          </span>
+          |
+          <span onClick={()=>{}}style={{paddingLeft: '10px'}}>
+            Add Answer
+          </span>
+        </div>
+      </div>
+      {question.firstTwoAnswers.map(oneAnswer => (
+        <div key={oneAnswer.id}>
+          <h3>
+            A:{' '}{oneAnswer.body}
+          </h3>
+          <div>
+            <span>
+              {`by ${oneAnswer.answerer_name}, ${oneAnswer.date} `}
+            </span>
+              | Helpful?
+              {' '}
+              <span
+                type='button'
+                onClick={()=>{console.log('click')}}
+                style={{ textDecoration: "underline" }}
+              >
+                Yes
+              </span>
+              {` (${oneAnswer.helpfulness}) | `}
+              <span
+                type='button'
+                onClick={()=>{console.log('click')}}
+                style={{ textDecoration: "underline"}}
+                >
+                  Report
+                </span>
+          </div>
+        </div>
+      ))}
+      {showMoreAs && question.restOfAnswers.map(oneAnswer => (
+        <div key={oneAnswer.id}>
+          <h3>
+            A:{' '}{oneAnswer.body}
+          </h3>
+          <div>
+            <span>
+              {`by ${oneAnswer.answerer_name}, ${oneAnswer.date} `}
+            </span>
+              | Helpful?
+              {' '}
+              <span
+                type='button'
+                onClick={()=>{console.log('click')}}
+                style={{ textDecoration: "underline" }}
+              >
+                Yes
+              </span>
+              {` (${oneAnswer.helpfulness}) | `}
+              <span
+                type='button'
+                onClick={()=>{console.log('click')}}
+                style={{ textDecoration: "underline"}}
+                >
+                  Report
+                </span>
+          </div>
+        </div>
+      ))}
+      {question.restOfAnswers.length ? (
+        <div>
+          <span
+          type="button"
+          onClick={changeQuestions}
+          >
+            {`${showMoreAs ? 'Hide' : 'Load More'} Answers`}
+          </span>
+        </div>
+      ) : null }
+    </>
+  )
+}
+
+export default Question;
